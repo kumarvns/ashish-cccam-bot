f805b3453e34974db4abd2a129ad669b644072b4
kumarvns <41120065+kumarvns@users.noreply.github.com>
2021-07-24 11:37:09 +0530

    Some minor fixes..

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import os
 from zeep import Client
+from PIL import Image
 
 def cline():
     chrome_options = webdriver.ChromeOptions()
@@ -20,15 +21,41 @@ def cline():
     element = driver.find_element_by_id("pin")
     element.screenshot("./sc/captcha.png")
 
+
+
+
     #now editing captcha images
-    os.system("python3 captcha.py")
+    image = Image.open("./sc/captcha.png")
+    image = image.crop((836, 483, 962, 520))  # defines crop points
+    image.save("./sc/captch.png", 'png')  # saves new cropped image
+
+    im = Image.open("./sc/captcha.png")
+    im = im.convert("P")
+    im2 = Image.new("P", im.size, 255)
+
+    im = im.convert("P")
+
+    temp = {}
+
+    for x in range(im.size[1]):
+        for y in range(im.size[0]):
+            pix = im.getpixel((y, x))
+            temp[pix] = pix
+            for i in range(2, 100):
+                if pix == i:
+                    im2.putpixel((y, x), 0)
+
+    im2.save("./sc/output.png")
+
+
+
 
     #--------------------------------------------------------------
     LOGIN = os.environ.get("LOGIN_CAP")
     LICENSE = os.environ.get("LIC_KEY")
     client = Client('http://www.ocrwebservice.com/services/OCRWebService.asmx?WSDL')
 
-    FilePath = "./sc/output.png"
+    FilePath = "./sc"
     with open(FilePath, 'rb') as image_file:
         image_data = image_file.read()
 
